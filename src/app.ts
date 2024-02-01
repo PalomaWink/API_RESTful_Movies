2edf5e2db235b240bcd0e4663ddfe45fd7d9ca71
Paloma Wink <paloma.wink@hotmail.com>
2024-02-01 20:42:59 -0300

    routes: Configurando o Express para criação das rotas

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log("Teste!");
+import express from "express";
+import config from "config";
+
+import router from "./router";
+
+const app = express();
+
+app.use(express.json())
+
+app.use("/api/", router);
+
+//app port
+const port = config.get<number>("port")
+
+app.listen(3000, async () => {
+  console.log(`Rodando na porta ${port}`);
+  
+})
