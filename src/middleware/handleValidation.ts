d1ebfb2e1b55d59428af31bb2742f6080934ba10
Paloma Wink <paloma.wink@hotmail.com>
2024-03-24 15:43:22 -0300

    feat: criando um middleware coringa para lidar com os erros da aplicação

---
+++ src/middleware/handleValidation.ts
@@ -0,0 +1,22 @@
+import { log } from "console";
+import { Request, Response, NextFunction } from "express";
+import { validationResult } from "express-validator";
+
+export const validate = (req: Request, res: Response, next: NextFunction) => {
+
+  const errors = validationResult(req);
+  
+
+  if (errors.isEmpty()) {
+    return next();
+  }
+
+  const extractedErrors: object[] = [];
+  // estou add os errors dentro de um array para conseguir retornar todos os erros de uma vez
+  errors.array().map(err => extractedErrors.push({ [err.param]: err.msg }));
+
+  return res.status(422).json({
+    errors: extractedErrors,
+  });
+
+};
--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,11 @@
 import { Router, Request, Response } from "express";
 import { createMovie } from "./controllers/movieController";
 
+//validations
+import { validate } from "./middleware/handleValidation";
+
 const router = Router()
 
 export default router.get('/test', (req: Request, res: Response) => {
   return res.status(200).send("API funcionando!")
-}).post("/movie", createMovie)
+}).post("/movie", validate, createMovie)
