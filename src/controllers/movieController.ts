d94077ffb1f785e82914d358ca64a2581cc57a43
Paloma Wink <paloma.wink@hotmail.com>
2024-03-24 21:03:01 -0300

    routes: Create getAll, update e delete para as rotas de movies

--- src/controllers/movieController.ts
+++ src/controllers/movieController.ts
@@ -21,10 +21,45 @@ export async function findMovieById(req: Request, res: Response) {
     const id = req.params.id;
     const movie = await MovieModel.findById(id);
     if (!movie) {
-      return res.status(404).json({ message: "Movie not found" });
+      return res.status(404).json({ error: "Movie not found" });
     }
     return res.status(200).json(movie);
   } catch (error: any) {
     Logger.error(`Error finding movie: ${error.message}`);
+  }
+}
+
+export async function getAllMovies(_req: Request, res: Response) {
+  try {
+    const movies = await MovieModel.find();
+    return res.status(200).json(movies);
+  } catch (error: any) {
+    Logger.error(`Error finding movies: ${error.message}`);
+  }
+}
+
+export async function removeMovie(req: Request, res: Response) {
+  try {
+    const id = req.params.id;
+    const movie = await MovieModel.findByIdAndDelete(id);
+    if (!movie) {
+      return res.status(404).json({ error: "Movie not found" });
+    }
+    return res.status(200).json(movie);
+  } catch (error: any) {
+    Logger.error(`Error removing movie: ${error.message}`);
+  }
+}
+
+export async function updateMovie(req: Request, res: Response) {
+  try {
+    const id = req.params.id;
+    const movie = await MovieModel.findByIdAndUpdate(id, req.body);
+    if (!movie) {
+      return res.status(404).json({ error: "Movie not found" });
+    }
+    return res.status(200).json(movie);
+  } catch (error: any) {
+    Logger.error(`Error updating movie: ${error.message}`);
   }
 }
--- src/router.ts
+++ src/router.ts
@@ -1,14 +1,18 @@
 import { Router, Request, Response } from "express";
-import { createMovie, findMovieById } from "./controllers/movieController";
+import { createMovie, findMovieById, getAllMovies, removeMovie, updateMovie } from "./controllers/movieController";
 
 //validations
 import { validate } from "./middleware/handleValidation";
+import { movieCreateValidation } from "./middleware/movieValidation";
 
 const router = Router()
-
+// Qaudno temos verbos HTTP que são diferentes podemos utilizar a mesma rota.
 export default router
   .get('/test', (req: Request, res: Response) => {
     return res.status(200).send("API funcionando!")
   })
   .post("/movie", validate, createMovie)
   .get("/movie/:id", findMovieById)
+  .get("/movies", getAllMovies)
+  .delete("/movie/:id", removeMovie)
+  .patch("/movie/:id", validate, movieCreateValidation, updateMovie)
